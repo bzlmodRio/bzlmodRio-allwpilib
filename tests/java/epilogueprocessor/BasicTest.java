0631ffe28c51cc8118661fc824b49e267f648345
PJ Reiniger <pj.reiniger@gmail.com>
2025-12-03 05:24:11 -0500

    Lint

--- tests/java/epilogueprocessor/BasicTest.java
+++ tests/java/epilogueprocessor/BasicTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class BasicTest {
-
   @Test
   void basicTest() {
     assertTrue(true); // TODO make test
