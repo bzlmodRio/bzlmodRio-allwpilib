cfdc7195833cdf5d816ec15a59215cadf846c963
PJ Reiniger <pj.reiniger@gmail.com>
2026-06-07 23:43:07 -0400

    Update gen

--- generate/get_allwpilib_dependencies.py
+++ generate/get_allwpilib_dependencies.py
@@ -108,7 +108,7 @@ def _executable_tool(
 def get_allwpilib_dependencies(
     use_local_opencv=False,
     use_local_ni=False,
-    opencv_version_override="2025.4.10.0-3.bcr3",
+    opencv_version_override="2027.4.13.0-3",
 ):
     year = "2027"
     version = "2027.0.0-alpha-6"
